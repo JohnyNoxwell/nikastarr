3404a29051c7a0d13a85d544b02f1e3b4e4abe6a
Johnynoxwell <jimboo19898@gmail.com>
2018-10-08 01:40:03 +0300

    fox

--- gulpfile.js
+++ gulpfile.js
@@ -40,6 +40,7 @@ gulp.task('js', ['common-js'], function() {
 	return gulp.src([
 		// 'app/libs/jquery/dist/jquery.min.js',
 		'app/libs/lightslider.js',
+		'app/libs/fullpage.js',
 		'app/js/common.min.js', // Всегда в конце
 		])
 	.pipe(concat('scripts.min.js'))
@@ -53,7 +54,7 @@ gulp.task('sass', function() {
 	.pipe(sass({outputStyle: 'expanded'}).on("error", notify.onError()))
 	.pipe(rename({suffix: '.min', prefix : ''}))
 	.pipe(autoprefixer(['last 15 versions']))
-	.pipe(cleanCSS()) // Опционально, закомментировать при отладке
+	// .pipe(cleanCSS()) // Опционально, закомментировать при отладке
 	.pipe(gulp.dest('app/css'))
 	.pipe(browserSync.stream())
 });
